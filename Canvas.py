a5a79c83f27a145a2a36cfa7f09b215449e0da71
Matt Cromwell <66025680+mattcrom15@users.noreply.github.com>
2020-08-26 17:00:14 +0100

    added alpha (#1)
    * added alpha
    
    * new alpha shader
    
    Co-authored-by: Matt C <matt@markcunniffe.com>

--- Canvas.py
+++ Canvas.py
@@ -2,8 +2,6 @@ import c4d
 from c4d import gui, storage
 import os
 
-#Welcome to the world of Python
-
 
 def main():
     #load image from file loaction
@@ -11,13 +9,16 @@ def main():
     path, filename = os.path.split(img)
     #get filename
     fname, ext = filename.split('.')
+
     
-    
+ 
+   
     #create material
     mat = c4d.BaseMaterial(5703)    ##create material
     msg = gui.RenameDialog('Material Name?')
     mat[c4d.MATERIAL_USE_COLOR] = False
     mat[c4d.MATERIAL_USE_LUMINANCE] = True
+    mat[c4d.MATERIAL_USE_ALPHA] = True
     mat[c4d.MATERIAL_USE_REFLECTION] = False
     mat[c4d.MATERIAL_PREVIEWSIZE] = + 12
     mat[c4d.MATERIAL_ANIMATEPREVIEW] = True
@@ -29,11 +30,23 @@ def main():
     mat[c4d.MATERIAL_LUMINANCE_SHADER]= shdr_texture        
     mat.InsertShader(shdr_texture)
     
+    
     bm = c4d.bitmaps.BaseBitmap()
     bm.InitWith(img)
     getsize = bm.GetSize()
     x = getsize[0]
     y = getsize[1]
+    
+    c = bm.GetChannelCount()
+    print(c)
+    
+    if c > 0:
+        alpha_texture = c4d.BaseList2D(c4d.Xbitmap)    ##create shader
+        alpha_texture[c4d.BITMAPSHADER_FILENAME] = img    ##add image to shader 
+        mat[c4d.MATERIAL_ALPHA_SHADER]= alpha_texture        
+        mat.InsertShader(alpha_texture)
+    else:
+        pass
 
     
     plane = c4d.BaseObject(5168)
@@ -58,6 +71,8 @@ def main():
     ##update undos
     doc.AddUndo(c4d.UNDOTYPE_NEW, plane)
     doc.AddUndo(c4d.UNDOTYPE_NEW, mat)
+    doc.AddUndo(c4d.UNDOTYPE_NEW, alpha_texture)
+    doc.AddUndo(c4d.UNDOTYPE_NEW, shdr_texture)
     doc.EndUndo()
     ##add to file
     c4d.EventAdd()
